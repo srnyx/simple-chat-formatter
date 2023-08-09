117a5f65fccd1f5d374e7ed0da6ea58c521c407c
srnyx <25808801+srnyx@users.noreply.github.com>
2023-08-09 11:55:22 -0400

    Bump version (1.1.0)

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
     id("com.github.johnrengelman.shadow") version "8.1.1"
 }
 
-setupAnnoyingAPI("4.1.0", "xyz.srnyx", "1.0.0", "A very simple plugin to format your chat!")
+setupAnnoyingAPI("4.1.0", "xyz.srnyx", "1.1.0", "A very simple plugin to format your chat!")
 spigotAPI("1.8.8")
 repository(Repository.PLACEHOLDER_API)
 dependencies.compileOnly("me.clip", "placeholderapi", "2.11.3")
