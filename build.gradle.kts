bf04b7e6320a39724ea92110cf0c466d718ce266
srnyx <25808801+srnyx@users.noreply.github.com>
2024-11-17 14:02:46 -0500

    Update Gradle and plugins
    Gradle: 8.1.1 -> 8.11
    Gradle Galaxy: 1.1.2 -> 1.3.2
    Shadow: 8.1.1 -> 8.3.5

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,8 +6,8 @@ import xyz.srnyx.gradlegalaxy.utility.spigotAPI
 
 plugins {
     java
-    id("xyz.srnyx.gradle-galaxy") version "1.1.2"
-    id("com.github.johnrengelman.shadow") version "8.1.1"
+    id("xyz.srnyx.gradle-galaxy") version "1.3.2"
+    id("com.gradleup.shadow") version "8.3.5"
 }
 
 setupAnnoyingAPI("a7ca4162e8", "xyz.srnyx", "1.1.0", "A very simple plugin to format your chat!")
